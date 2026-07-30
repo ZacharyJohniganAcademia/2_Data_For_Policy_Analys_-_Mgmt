56a8ad57f0fa2b4da767cf6e0c643933140365e1
ZacharyJohniganAcademia <zacharyjohniganacademia@gmail.com>
2026-07-30 16:55:47 -0500

    ...

--- Labs/Lab_4/Lab4_blank.R
+++ Labs/Lab_4/Lab4_blank.R
@@ -12,14 +12,17 @@
 # Set up working space and import packages
 #------------------------------------------------------------------------------#
 
+# This cleans up everything in the "Environment" pane.
 rm(list=ls())
 options("error") 
 options(lifecycle_disable_verbose_retirement = TRUE)
 
 # Import packages
-
 package.list <- c("tidyverse")
 
+# read the `package.list` for the list of packages we will be needing
+# Check what packages are installed installed arleady 
+# If any of the packages in the c() list are not currently installed, install them.
 for (p in package.list){
   if (!p %in% installed.packages()[, "Package"]) install.packages(p)
   library(p, character.only=TRUE)
